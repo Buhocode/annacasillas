9e677f68cd5078c1eca2cadad6cb04c49f3e6b98
Sebastian <37258153+PenguinDesign@users.noreply.github.com>
2020-06-11 12:08:50 -0500

    Svelte estructura

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,47 @@
+<script>
+  // para poder mandar info de navegacion
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+
+  let selectedNav = "home";
+
+  // manda info de cual navegacion tiene
+  const changeNav = path => {
+    selectedNav = path;
+    dispatch("message", {
+      text: selectedNav
+    });
+  };
+
+  let navs = [
+    "home",
+    "productos",
+    "retratos",
+    "proyectos",
+    "naturaleza",
+    "contacto"
+  ];
+</script>
+
+<style>
+  .navbar {
+    display: flex;
+  }
+  .nav-item {
+    padding: 0 1rem;
+    cursor: pointer;
+  }
+  .nav-item:active {
+    background-color: blue;
+  }
+</style>
+
+<nav class="navbar">
+  <!-- Por cada nombre en navs, crea una navegacion -->
+  {#each navs as navItem}
+    <div on:click={() => changeNav(navItem)} class="nav-item">
+      {navItem.toUpperCase()}
+    </div>
+  {/each}
+
+</nav>
---
+++ src/pages/Home.svelte
@@ -0,0 +1,4 @@
+<section>
+    <h1>Anna Casillas</h1>
+    <p>Bienvenido a la pagina de una fotografa chida</p>
+</section>
